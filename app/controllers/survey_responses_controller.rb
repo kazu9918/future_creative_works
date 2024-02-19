7d7c422255048b5be8c287d5b2c34a86cc7f7d60
Zuyama <zuyama.tomoya@tpj.co.jp>
2024-02-19 23:39:41 +0900

    アンケート画面からの情報をDBに保存する処理を追加

--- app/controllers/survey_responses_controller.rb
+++ app/controllers/survey_responses_controller.rb
@@ -1,10 +1,26 @@
+# app/controllers/survey_responses_controller.rb
+
 class SurveyResponsesController < ApplicationController
   def new
+    @survey_response = SurveyResponse.new
+    # 必要なビューロジックを追加する場合はここに追加
   end
 
   def create
+    @survey_response = SurveyResponse.new(survey_response_params)
+    if @survey_response.save
+      flash[:success] = "アンケートが送信されました。"
+      redirect_to root_path
+# 　　　アンケートご協力感謝画面に遷移するよう変更が必要
+    else
+      flash[:error] = "アンケートの送信中にエラーが発生しました。"
+      render :new
+    end
   end
 
-  def thank_you
+  private
+
+  def survey_response_params
+    params.require(:survey_response).permit(:gender, :age_group, :years_of_service, :response_date, :days_with_symptoms, :task_volume_completion_level, :task_quality_level, :company_id, :department_id, :employment_type_id, :job_function_id)
   end
 end
---
+++ db/migrate/20240219141251_create_survey_responses.rb
@@ -0,0 +1,19 @@
+class CreateSurveyResponses < ActiveRecord::Migration[6.0]
+  def change
+    create_table :survey_responses do |t|
+      t.string :gender
+      t.string :age_group
+      t.integer :years_of_service
+      t.datetime :response_date
+      t.integer :days_with_symptoms
+      t.integer :task_volume_completion_level
+      t.integer :task_quality_level
+      t.references :company, foreign_key: true
+      t.references :department, foreign_key: true
+      t.references :industry, null: false
+      t.references :job_function, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
